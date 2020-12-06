5d1f04c748e28455bf64612d9f1d15a0cb19a76f
mingfanghu123 <47246993+mingfanghu123@users.noreply.github.com>
2020-12-06 17:00:22 -0500

    Add files via upload

---
+++ include/ui.h
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef UI_H
+#define UI_H
+
+#include <string.h>
+#include <stdio.h>
+
+#include "stu.h"
+#include "user.h"
+
+#define CLEAR_BUF \
+    {\
+    int ch; \
+while ((ch = getchar()) != EOF && ch != '\n')\
+        {\
+        ; \
+        }\
+    }
+
+
+
+void get_pass(char* password);
+
+void route_admin(p_user_t*, p_stu_t*, char*, char*);
+
+void route_guest(p_stu_t);
+
+void ui_init();
+
+void ui_admin();
+
+void ui_guest();
+
+void ui_stu_query(int);
+
+void ui_user_query();
+
+void ui_exit();
+
+#endif
