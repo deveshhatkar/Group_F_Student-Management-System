f701a00fa7e6017e7199dd937a7acd131a84fcbf
Devesh Hatkar <deveshhatkar@gmail.com>
2020-12-07 11:59:05 -0500

    Adding file with empty functions, implementations on the way!

---
+++ src/db.c
@@ -0,0 +1,30 @@
+//These are just empty functions. Implementations are on the way!
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include "db.h"
+#include "user.h"
+#include "stu.h"
+#include "ui.h"
+
+int app_start(char* conf_filename)
+{
+	return 0;
+}
+
+void init_env(p_user_t* user_list, p_stu_t* stu_list, char* str1, char* str2)
+{
+	return;
+}
+
+void studb_update(p_stu_t*, char*)
+{
+	return;
+}
+
+void userdb_update(p_user_t*, char*)
+{
+	return;
+}
