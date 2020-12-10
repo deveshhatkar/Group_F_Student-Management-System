71b937ea0029d304d6ca1c48d46a725dcde15a04
Devesh Hatkar <deveshhatkar@gmail.com>
2020-12-10 00:46:33 -0500

    Temp devesh (#17)
    Merge

--- include/db.h
+++ include/db.h
@@ -2,15 +2,10 @@
 #ifndef DB_H
 #define DB_H
 
-#include "user.h"
 #include "stu.h"
 
 int app_start(char* conf_filename);
 
-void init_env(p_user_t* user_list, p_stu_t* stu_list, char* str1, char* str2);
-
-void studb_update(p_stu_t*, char*);
-
-void userdb_update(p_user_t*, char*);
+void init_env(p_stu_t* stu_list);
 
 #endif // !DB_H
--- src/db.c
+++ src/db.c
@@ -1,30 +1,45 @@
-//These are just empty functions. Implementations are on the way!
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
 #include "db.h"
-#include "user.h"
 #include "stu.h"
 #include "ui.h"
 
-int app_start(char* conf_filename)
+int app_start(char* conf_file)
 {
+	if (strlen(conf_file) > 0) {
+		printf("The program will run in memory-based mode. Configurations will be ignored.");
+	}
+
+	stu_t* stu_list = NULL;
+
+	init_env(&stu_list);
+	while (1) {
+		system("cls");
+		ui_init();
+		printf("Please enter a number to proceed: ");
+		int button = ui_get_option();
+		if (button == '1') {
+			ui_exit();
+			return 0;
+		}
+		else if (button == '0') {
+			route_admin(&stu_list);
+		}
+		else {
+			ui_invalid_prompt();
+			continue;
+		}
+	}
 	return 0;
 }
 
-void init_env(p_user_t* user_list, p_stu_t* stu_list, char* str1, char* str2)
+void init_env(p_stu_t* stu_list)
 {
-	return;
-}
-
-void studb_update(p_stu_t*, char*)
-{
-	return;
-}
-
-void userdb_update(p_user_t*, char*)
-{
-	return;
+	stu_t* stu_head = (p_stu_t)calloc(1, sizeof(stu_t));
+	stu_t* pstu = NULL, * stu_tail = stu_head, * pre = NULL, * pcur = NULL, tmp_stu;
+	stu_t* pvisit = NULL;
+	int i;
+	*stu_list = stu_head;
+	memset(&tmp_stu, 0, sizeof(stu_t));
 }
