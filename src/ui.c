6ebcb0d7dbc5d4a6eba4e203b50bec7448453583
mingfanghu123 <47246993+mingfanghu123@users.noreply.github.com>
2020-12-09 22:17:42 -0500

    Update ui.c

--- src/ui.c
+++ src/ui.c
@@ -1,56 +1,104 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
 #include "ui.h"
 #include "user.h"
 #include "stu.h"
+#include "platform.h"
 
-void ui_init()
-{
-	printf("welcome to Student Record Management System!");
-	printf("\nChoose your account type:\n\n");
-	printf("[0] Admin\n");
-	printf("[1] Student\n");
-	printf("[2] Exit\n\n");
-}
+#define UI_INPUT_BUFFER_LEN 65536
+static char ui_input_buffer[UI_INPUT_BUFFER_LEN];
 
 void get_pass(char* password)
 {
-	return;
+	// Todo
 }
 
-void route_admin(p_user_t* user_list, p_stu_t* stu_list, char* str1, char* str2)
+void ui_cls()
 {
-	return;
+	system(CLS_COMMAND);
 }
 
-void route_guest(p_stu_t stu_list) 
+void ui_pause()
 {
-	return;
+	gets(ui_input_buffer);
 }
+
+void ui_invalid_prompt()
+{
+	printf("Invalid input! Press ENTER to continue.\n");
+	gets(ui_input_buffer);
+}
+
+char ui_get_option()
+{
+	for (;;) {
+		gets(ui_input_buffer);
+		if (strlen(ui_input_buffer) == 1) {
+			return ui_input_buffer[0];
+		}
+	}
+}
+
+void ui_init()
+{
+	printf("welcome to Student Record Management System!\n");
+	printf("[0] Enter System\n");
+	printf("[1] Exit\n\n");
+}
+
+void route_admin(p_stu_t* stu_list)
+{
+	char button;
+	while (1) {
+		ui_cls();
+		ui_admin();
+		printf("Please enter a number:");
+		button = ui_get_option();
+		switch (button) {
+		case '1':stuinfo_add(stu_list); break;
+		case '2':stuinfo_del(stu_list); break;
+		case '3':stuinfo_update(stu_list); break;
+		case '4':stuinfo_query(*stu_list);   break;
+		case '5':ui_exit(); break;
+		default: ui_invalid_prompt();
+		}
+	}
+}
+
 
 void ui_admin()
 {
-	return;
+	printf("[1] %-8s new student record\n", "add");
+	printf("[2] %-8s existed student record\n", "delete");
+	printf("[3] %-8s existed student record\n", "modify");
+	printf("[4] %-8s student record\n", "search");
+	printf("[5] exit\n");
 }
 
-void ui_guest()
+void ui_stu_query()
 {
-	return;
-}
+	ui_cls();
+	printf("[0] Print all student record\n");
+	printf("[1] Search student record by ID\n");
+	printf("[2] Search student record by name\n");
+	printf("[3] Back\n\n");
 
-void ui_stu_query(int limit)
-{
-	return;
 }
 
 void ui_user_query()
 {
-	return;
+	ui_cls();
+	printf("[0] Print all account record\n");
+	printf("[1] search user account record by user type\n");
+	printf("[2] search user account record by user name\n");
+	printf("[3] Back\n\n");
 }
 
 void ui_exit()
 {
-	return;
+	ui_cls();
+	printf("Thank you for using student record management system!\n");
+	ui_pause();
+	exit(0);
 }
