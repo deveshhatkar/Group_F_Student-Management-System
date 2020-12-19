c341669ce35856317615a9cd34db4ab913502688
Devesh Hatkar <deveshhatkar@gmail.com>
2020-12-19 02:34:18 -0500

    Added function to load the Student and User details from File into the System. Also, added configuration, student and user files to and from which this data is exchanged.

--- include/db.h
+++ include/db.h
@@ -8,8 +8,8 @@ int app_start(char* conf_filename);
 
 void init_env(p_stu_t* stu_list);
 
-void studb_update(p_stu_t* stu_list);
+void studb_update(p_stu_t* stu_list, char* str);
 
-void userdb_update(p_user_t* user_list);
+void userdb_update(p_user_t* user_list, char* str);
 
 #endif // !DB_H
